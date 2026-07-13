c70aa5926c3ebc9eff6e46719d0cd8888b58bf30
MSM1905 <1162648613@qq.com>
2026-07-13 16:43:21 +0800

    Clean up init_db function by removing comment
    Removed unnecessary comment in init_db function.

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import sqlite3
 DB_NAME = "saiyan_origin.db"
 
 def init_db():
-    """初始化数据库"""
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS custom_rules (
